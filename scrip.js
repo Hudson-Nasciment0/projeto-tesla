bffadd0ab20ea0ea53174d98b3684929cd4d6e1a
Hudson-Nasciment0 <nascimentoh714@gmail.com>
2024-07-12 00:38:07 -0300

    primeiro commit do projeto tesla

---
+++ scrip.js
@@ -0,0 +1,18 @@
+// Variáveis: uma forma DE IDENTIFICAR QUEM É QUEM;
+//DA UM NOME PRA CADA UM PEDACINHO DE CÓDIGO;
+//É COMO SE VC CRIASSE UMA PASTA, PARA GUARDAR O QUE QUISER.
+
+// Funções : É um trecho de código que só executa, qunado é chamado.
+//
+
+
+//ducoment = ir no HTML
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "35%"
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-75%"
+}
